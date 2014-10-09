b7ca8f7bfe6bfa57d28894c1801328c71a3d145f
Andrew de Andrade <aandrade@uber.com>
2014-10-08 20:36:16 -0700

    Updating all dependencies

--- set-resolved.js
+++ set-resolved.js
@@ -10,6 +10,7 @@ var version = require('./package.json').version;
 
 var NPM_URI = 'https://registry.npmjs.org/{name}/-/{name}-{version}.tgz';
 var INVALID_VERSION = TypedError({
+    type: 'npm-shrinkwrap.invalid_version',
     message: 'Using an older version of npm-shrinkwrap.\n' +
         'Expected version {existing} but found {current}.\n' +
         'To fix: please run `npm install npm-shrinkwrap@{existing}`\n'
